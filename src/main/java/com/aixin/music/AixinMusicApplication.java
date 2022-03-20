f6c786ecb96cc53ba1d042416cae58016c77c7e6
AI Xin <xin.ai@hairobotics.com>
2022-03-20 21:55:51 +0800

    wip

---
+++ src/main/java/com/aixin/music/AixinMusicApplication.java
@@ -0,0 +1,11 @@
+package com.aixin.music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AixinMusicApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AixinMusicApplication.class, args);
+    }
+}
---
+++ src/main/java/com/aixin/music/controller/DefaultController.java
@@ -0,0 +1,16 @@
+package com.aixin.music.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class DefaultController {
+
+    @GetMapping
+    public String sayHello()
+    {
+        return "Hello";
+    }
+}
